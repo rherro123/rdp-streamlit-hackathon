69b7c45cb22f22bcd5d8592a3d4426d922378ab4
bxk762 <Brandon.Kedson@sherwin.com>
2025-08-07 15:17:45 -0400

    Remove broken logging

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -2,15 +2,6 @@ import traceback
 
 import streamlit as st
 
-from streamlit_app._logger import log, logging_config
-
-
-@st.cache_resource
-def _configure_logging() -> None:
-    logging_config()
-    log.info("Starting application...")
-
-
 def main() -> None:
     welcome = st.Page("pages/welcome.py", title="Welcome", icon=":material/home:", default=True)
 
@@ -26,8 +17,6 @@ def main() -> None:
 
 if __name__ == "__main__":
     try:
-        _configure_logging()
         main()
     except Exception as e:
-        log.critical(traceback.format_exc())
         raise e
